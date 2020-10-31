eddbea1ad9aa6436b61e9efb60c67d78804c3a7a
Suraj Shrestha <syuraj@gmail.com>
2020-10-31 16:51:37 -0400

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {BottomTabs} from './bottom-tabs';
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
 
@@ -47,12 +19,6 @@ function App({initialScreenName}) {
           component={BottomTabs}
           initialParams={{initialScreenName}}
         />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
