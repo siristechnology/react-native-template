5af2564cb1dbbf8f233ff6ac872afefb3fc4076f
Suraj Shrestha <syuraj@gmail.com>
2020-10-31 22:03:06 -0400

    name change

--- android/app/src/main/java/com/siristechnology/rntemplate/MainApplication.java
+++ android/app/src/main/java/com/siristechnology/rntemplate/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -63,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.template.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.siristechnology.rntemplate.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
---
+++ prepare_release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="com.siristechnology.rntemplate"
+APP_NAME="RNTemplate"
+TIME_STAMP=$(date +%s)
+IC_LAUNCHER_BACKGROUND="#E7F3FF"
+BUILD_GRADLE=./android/app/build.gradle
+ANDROID_STRINGS_FILE=./android/app/src/main/res/values/strings.xml
+ANDROID_MANIFEST_FILE=./android/app/src/main/AndroidManifest.xml
+IC_LAUNCHER_BACKGROUND_FILE=./android/app/src/main/res/values/ic_launcher_background.xml
+
+GOOGLE_SERVICES_FILE=./android/app/google-services.json
+
+sed -i '' -e 's/applicationId "[^"]*"/applicationId "'$PACKAGE_NAME'"/' $BUILD_GRADLE
+sed -i '' -e 's/versionCode .*$/versionCode '$TIME_STAMP'/' $BUILD_GRADLE
+sed -i '' -e 's/"app_name">[^<]*</"app_name">'$APP_NAME'</' $ANDROID_STRINGS_FILE
+
+sed -i '' -e 's/"ic_launcher_background">[^<]*</"ic_launcher_background">'$IC_LAUNCHER_BACKGROUND'</' $IC_LAUNCHER_BACKGROUND_FILE
+
+mv .env.release .env
+mv ./android/app/google-services.json.prod ./android/app/google-services.json
